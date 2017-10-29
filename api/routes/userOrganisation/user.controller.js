aa319c4749bc2e4d5451c5f2ec047f271cd11e9f
Piotrek <piotr.pietruszka@o2.pl>
2017-10-29 04:08:58 +0200

    400

--- api/routes/userOrganisation/user.controller.js
+++ api/routes/userOrganisation/user.controller.js
@@ -9,10 +9,12 @@ const registerUser = (req, res, next) => {
     req.checkBody('password', 'Invalid postparam').notEmpty();
     req.checkBody('password', 'Invalid password length').len(6,40);
     req.checkBody('name', 'Invalid postparam').notEmpty();
-    req.checkBody('surname', 'Invalid postparam').notEmpty();
+    req.checkBody('personname', 'Invalid postparam').notEmpty();
     req.checkBody('phone', 'Invalid postparam').notEmpty();
     req.checkBody('address', 'Invalid postparam').notEmpty();
     req.checkBody('zipcode', 'Invalid postparam').notEmpty();
+    req.checkBody('city', 'Invalid postparam').notEmpty();
+    req.checkBody('nip', 'Invalid postparam').notEmpty();
 
     req.getValidationResult()
         .then((result)=>{
